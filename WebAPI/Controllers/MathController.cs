09caa74a63214ecc5fa8d211aff162798ea4d171
johnsonchou3 <johnsonchou3@gmail.com>
2021-07-14 16:49:19 +0800

    Initial commit

---
+++ WebAPI/Controllers/MathController.cs
@@ -0,0 +1,26 @@
+﻿//MathController.cs file.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+
+namespace WebAPI.Controllers
+{
+    public class MathController : ApiController
+    {
+        public string Compute(string oper)
+        {
+            DataTable dt = new DataTable();
+            return dt.Compute(oper,"").ToString();
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "default";
+        }
+    }
+}
